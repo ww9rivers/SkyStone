4ab8d89713852217a9871868235ca9f4ec8d49fe
Fansheng Meng <fshmeng@gmail.com>
2019-12-30 21:26:53 -0500

    First worked controller with OpenCV supported

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
@@ -129,82 +129,107 @@ import org.opencv.android.OpenCVLoader;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
+import ftc.vision.BeaconProcessor;
 import ftc.vision.FrameGrabber;
 
 @SuppressWarnings("WeakerAccess")
 public class FtcRobotControllerActivity extends Activity
   {
-    ////////////// START VISION PROCESSING CODE //////////////
+      ////////////// START VISION PROCESSING CODE //////////////
 
-    // Loads camera view of OpenCV for us to use. This lets us see using OpenCV
-    private CameraBridgeViewBase cameraBridgeViewBase;
+      static final int FRAME_WIDTH_REQUEST = 176;
+      static final int FRAME_HEIGHT_REQUEST = 144;
 
-    void myOnCreate(){
-      getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+      // Loads camera view of OpenCV for us to use. This lets us see using OpenCV
+      private CameraBridgeViewBase cameraBridgeViewBase;
 
-      cameraBridgeViewBase = (JavaCameraView) findViewById(R.id.cameraSurfaceView);
-  //    if(cameraBridgeViewBase == null)
-  //      return;
-      new FrameGrabber(cameraBridgeViewBase);
-    }
+      //manages getting one frame at a time
+      public static FrameGrabber frameGrabber = null;
 
-    //when the "Grab" button is pressed
-    public void frameButtonOnClick(View v){
-    }
-
-    void myOnPause(){
-      if (cameraBridgeViewBase != null) {
-        cameraBridgeViewBase.disableView();
+      //set up the frameGrabber
+      void myOnCreate(){
+          getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+          cameraBridgeViewBase = (JavaCameraView) findViewById(R.id.cameraSurfaceView);
+          frameGrabber = new FrameGrabber(cameraBridgeViewBase, FRAME_WIDTH_REQUEST, FRAME_HEIGHT_REQUEST);
+          frameGrabber.setImageProcessor(new BeaconProcessor());
+          frameGrabber.setSaveImages(true);
       }
-    }
 
-    void myOnResume(){
-      if (!OpenCVLoader.initDebug()) {
-        Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
-        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
-      } else {
-        Log.d(TAG, "OpenCV library found inside package. Using it!");
-        mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+      //when the "Grab" button is pressed
+      public void frameButtonOnClick(View v){
+          frameGrabber.grabSingleFrame();
+          while (!frameGrabber.isResultReady()) {
+              try {
+                  Thread.sleep(5); //sleep for 5 milliseconds
+              } catch (InterruptedException e) {
+                  e.printStackTrace();
+              }
+          }
+          Object result = frameGrabber.getResult();
+          ((TextView)findViewById(R.id.resultText)).setText(result.toString());
       }
-    }
 
-    public void myOnDestroy() {
-      if (cameraBridgeViewBase != null) {
-        cameraBridgeViewBase.disableView();
+      void myOnWindowFocusChanged(boolean hasFocus){
+          if (hasFocus) {
+              frameGrabber.stopFrameGrabber();
+          } else {
+              frameGrabber.throwAwayFrames();
+          }
       }
-    }
 
-    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
-      @Override
-      public void onManagerConnected(int status) {
-        switch (status) {
-          case LoaderCallbackInterface.SUCCESS:
-            Log.i(TAG, "OpenCV Manager Connected");
-            //from now onwards, you can use OpenCV API
+      void myOnPause(){
+          if (cameraBridgeViewBase != null) {
+              cameraBridgeViewBase.disableView();
+          }
+      }
+
+      void myOnResume(){
+          if (!OpenCVLoader.initDebug()) {
+              Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+              OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+          } else {
+              Log.d(TAG, "OpenCV library found inside package. Using it!");
+              mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+          }
+      }
+
+      public void myOnDestroy() {
+          if (cameraBridgeViewBase != null) {
+              cameraBridgeViewBase.disableView();
+          }
+      }
+
+      private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+          @Override
+          public void onManagerConnected(int status) {
+              switch (status) {
+                  case LoaderCallbackInterface.SUCCESS:
+                      Log.i(TAG, "OpenCV Manager Connected");
+                      //from now onwards, you can use OpenCV API
 //          Mat m = new Mat(5, 10, CvType.CV_8UC1, new Scalar(0));
-            cameraBridgeViewBase.enableView();
-            break;
-          case LoaderCallbackInterface.INIT_FAILED:
-            Log.i(TAG, "Init Failed");
-            break;
-          case LoaderCallbackInterface.INSTALL_CANCELED:
-            Log.i(TAG, "Install Cancelled");
-            break;
-          case LoaderCallbackInterface.INCOMPATIBLE_MANAGER_VERSION:
-            Log.i(TAG, "Incompatible Version");
-            break;
-          case LoaderCallbackInterface.MARKET_ERROR:
-            Log.i(TAG, "Market Error");
-            break;
-          default:
-            Log.i(TAG, "OpenCV Manager Install");
-            super.onManagerConnected(status);
-            break;
-        }
-      }
-    };
+                      cameraBridgeViewBase.enableView();
+                      break;
+                  case LoaderCallbackInterface.INIT_FAILED:
+                      Log.i(TAG, "Init Failed");
+                      break;
+                  case LoaderCallbackInterface.INSTALL_CANCELED:
+                      Log.i(TAG, "Install Cancelled");
+                      break;
+                  case LoaderCallbackInterface.INCOMPATIBLE_MANAGER_VERSION:
+                      Log.i(TAG, "Incompatible Version");
+                      break;
+                  case LoaderCallbackInterface.MARKET_ERROR:
+                      Log.i(TAG, "Market Error");
+                      break;
+                  default:
+                      Log.i(TAG, "OpenCV Manager Install");
+                      super.onManagerConnected(status);
+                      break;
+              }
+          }
+      };
 
-    ////////////// END VISION PROCESSING CODE //////////////
+      ////////////// END VISION PROCESSING CODE //////////////
   public static final String TAG = "RCActivity";
   public String getTag() { return TAG; }
 
