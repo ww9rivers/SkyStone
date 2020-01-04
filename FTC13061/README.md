44b67bb39c3c8ba1fd2a1ab5a7d0f4b4f17b6656
Wei Wang <weiwang@umich.edu>
2020-01-03 20:12:12 -0500

    Migrating FTC13061 team code to submodule

--- FTC13061/README.md
+++
@@ -1,5 +0,0 @@
-# FTC13061
-
-The SKYSTONE (2019-2020) competition season code for team FTC-13061.
-
- 
--- FTC13061/src/main/java/org/firstinspires/ftc/teamcode/Aliance.java
+++
@@ -1,5 +0,0 @@
-package org.firstinspires.ftc.teamcode;
-
-public class Aliance {
-    public enum Color { BLUE, RED };
-}
